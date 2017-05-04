54ce492ae1298e82a895140ca36fc8c2931c8f43
elvece <lvcifferello@gmail.com>
2017-05-04 00:00:29 -0600

    added fun buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 // add scripts
 
-console.log("sanity check!");
+function getName(){
+  var name = prompt("What is your name?");
+
+  if (name != null) {
+      document.getElementById("name").innerHTML =
+      "Hello " + name + "!";
+  }
+}
+
+function colorChange(id){
+  var background = document.getElementById(id).style.backgroundColor;
+  if(background === 'rgb(1, 193, 140)'){
+    color = 'rgb(255,255,255)';
+  } else {
+    color = 'rgb(1, 193, 140)';
+  }
+  document.body.style.background = color;
+}
